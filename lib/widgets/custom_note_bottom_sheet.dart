1fe87b0ec4d1bc870ccd53617edea7cad454ecca
a7medbehiry <a7medbehiry@gmail.com>
2023-09-21 18:50:06 +0300

    keyboard padding

--- lib/widgets/custom_note_bottom_sheet.dart
+++ lib/widgets/custom_note_bottom_sheet.dart
@@ -23,8 +23,10 @@ class CustomNoteBottomSheet extends StatelessWidget {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading ? true : false,
             child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
+              padding: EdgeInsets.only(
+                left: 16,
+                right: 16,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
               ),
               child: SingleChildScrollView(
                 child: AddNoteForm(),
