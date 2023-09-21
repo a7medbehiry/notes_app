81309374326602c510dbce664323174ee58b683f
a7medbehiry <a7medbehiry@gmail.com>
2023-09-21 19:25:31 +0300

    create fetch all notes method

--- lib/cubits/read_note_cubit/read_note_cubit.dart
+++ lib/cubits/read_note_cubit/read_note_cubit.dart
@@ -1,9 +1,29 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notesapp/constants.dart';
 import 'package:notesapp/models/note_model.dart';
 
 part 'read_note_state.dart';
 
 class ReadNoteCubit extends Cubit<ReadNoteState> {
   ReadNoteCubit() : super(ReadNoteInitial());
+
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(
+        ReadNoteSuccess(
+          notes,
+        ),
+      );
+    } catch (e) {
+      emit(
+        ReadNoteFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/cubits/read_note_cubit/read_note_cubit.dart';
 import 'package:notesapp/widgets/custom_note_bottom_sheet.dart';
 import 'package:notesapp/widgets/notes_view_body.dart';
 
@@ -7,25 +9,28 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                16,
+    return BlocProvider(
+      create: (context) => ReadNoteCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  16,
+                ),
               ),
-            ),
-            context: context,
-            builder: (context) {
-              return CustomNoteBottomSheet();
-            },
-          );
-        },
-        child: Icon(Icons.add),
+              context: context,
+              builder: (context) {
+                return CustomNoteBottomSheet();
+              },
+            );
+          },
+          child: Icon(Icons.add),
+        ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
